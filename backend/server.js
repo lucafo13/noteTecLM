d2e172f63ff05a7a7e2b843c28d6e682fef6570a
lucafo13 <odavilucas587@gmail.com>
2026-07-04 17:33:39 -0300

    transferindo info

--- backend/server.js
+++ backend/server.js
@@ -2,21 +2,23 @@
 /* Bibliotequitas */
 import express from 'express';
 import cors from 'cors'
-import 'dotenv/config';
+import dotenv from 'dotenv'
 import { PDFParse } from 'pdf-parse';
 import fs from 'fs'
 import path from 'path';
 import multer from 'multer';
 import { fileURLToPath } from 'url';
+import axios from 'axios';
 
 /* aura 67 express necessidades */
 const app = express();
 app.use(express.json())
 app.use(cors())
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-
+console.log(process.env.APIAI || process.env.PORT || "abc")
 const $PORT = process.env.PORT || 3000
 
 
