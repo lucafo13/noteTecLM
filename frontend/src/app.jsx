671e8e91944115ae9fa0ded287ea440f3e828e37
lucafo13 <odavilucas587@gmail.com>
2026-07-05 16:09:20 -0300

    refazendo a src

---
+++ frontend/src/app.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import 'app.css'
+
+const app = () => {
+    return(
+        <>
+            <h1>Olá noteTec</h1>
+        </>
+    )
+}
+
+export default app
---
+++ frontend/src/main.jsx
@@ -0,0 +1,6 @@
+import app from "./app";
+import reactDom from 'react-dom/client'
+
+reactDom.createRoot(document.getElementById('root')).render(
+    <App />
+)
