b7ad0563dac52bf586504aec6a363c8bf284df80
luca_FO🙏 <odavilucas587@gmail.com>
2026-07-27 15:34:28 -0300

    Atualizar o app.jsx

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate, useNavigate} from 'react-router-dom'
 
-import Home from './pages/home'
+import Home from './pages/Home'
 import { Toaster } from '#components/ui/sonner'
 import Login from './pages/login'
 import Cadastro from './pages/Cadastro'
