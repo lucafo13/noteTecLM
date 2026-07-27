f02fa110180cca2cc1f548cd07720759ddc6d5eb
lucafo13 <odavilucas587@gmail.com>
2026-07-27 16:58:44 -0300

    backend sinc dnv

--- backend/routes/login/login.js
+++ backend/routes/login/login.js
@@ -35,7 +35,7 @@ router.post("/login", async (req, res) => {
     );
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
       path: '/'
