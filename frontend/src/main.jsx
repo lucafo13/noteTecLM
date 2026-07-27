b29437ee85900de8eb54ddd67eed83e74d21b9d4
lucafo13 <odavilucas587@gmail.com>
2026-07-26 23:45:10 -0300

    dormir

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,15 @@
 import App from "./App";
 import reactDom from 'react-dom/client';
-import './App.css' ;
+import './App.css';
 import { StrictMode } from "react";
-import './index.css'
-import 'react-icons'
+import './index.css';
+import 'react-icons';
 import { BrowserRouter } from "react-router-dom";
+
 reactDom.createRoot(document.getElementById('root')).render(
    <StrictMode>
       <BrowserRouter>
-    <App />
-   </BrowserRouter>
-    </StrictMode>
-)
+        <App />
+      </BrowserRouter>
+   </StrictMode>
+);
